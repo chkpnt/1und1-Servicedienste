be3c55a76bc5b87546a1cefac5a537ac63c6e764
Gregor Dschung <gregor@chkpnt.de>
2020-06-21 18:39:36 +0200

    Publish updates via Git

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("de.chkpnt.servicedienste")
+    id("org.ajoberstar.git-publish") version "2.1.3"
 }
 
 servicediensteDsl {
@@ -12,4 +13,14 @@ servicediensteMobilfunk {
     sourceUrl = "https://hilfe-center.1und1.de/bin_dea/article/793873/Mobile_Rufnummernliste_Service_und_Konferenzdienste.pdf"
     downloadTo = "build/1und1/Mobile_Rufnummernliste_Service_und_Konferenzdienste.pdf"
     jsonExportFile = "servicedienste-mobilfunk.json"
+}
+
+gitPublish {
+    repoUri.set("git@github.com:chkpnt/1und1-Servicedienste.git")
+    branch.set("master")
+    commitMessage.set("Update 1&1 Servicedienste")
+    preserve { include("**/*") }
+    contents {
+        from("servicedienste-*.json")
+    }
 }
